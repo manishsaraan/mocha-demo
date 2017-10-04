19e546eaac856b307721b303e05a363e3b097c68
manish kumar <manishkumar@mastersoftwaresolutions.com>
2017-10-04 17:51:52 +0530

    diff tests for addition and multiplication

--- test.js
+++ test.js
@@ -1,19 +1,37 @@
 var assert = require('assert');
 var calc = require('./calc.js');
 
-describe('Calculations Test',function(){
+describe('Calculations Tests',function(){
 
-  //describe tests here..
-  it('returns 1 + 1 = 2',function(done){
-       assert.equal(calc.add(1,1),2);
-       //invoke done
-       done();
-  });
+     describe("Addtion Tests",function(){
+		  //describe tests here..
+		  it('returns 1 + 1 = 2',function(done){
+		       assert.equal(calc.add(1,1),2);
+		       //invoke done
+		       done();
+		  });
 
-  it("returns 2*2 = 4",function(done){
-  	assert.equal(calc(2,2),4);
-  	//invoke done
-  	done();
-  })
+		  it("returns 1 + -1 = 0",function(done){
+		  	assert.equal(calc.add(1,-1),0);
+		  	//invoke done
+		  	done();
+		  });
+      });
+
+     describe("Multiplication Tests",function(){
+		  //describe tests here..
+		  it('returns 2 * 2 = 4',function(done){
+		       assert.equal(calc.mul(2,2),4);
+		       //invoke done
+		       done();
+		  });
+
+		  it("returns 0 * 4 = 0",function(done){
+		  	assert.equal(calc.mul(0,4),0);
+		  	//invoke done
+		  	done();
+		  });
+      });
+
 
 });
